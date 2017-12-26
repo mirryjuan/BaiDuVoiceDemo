a642a3e443e96878f6de14020c8b1e6250c1b6cd
mirryjuan <1599270722@qq.com>
2017-12-26 10:11:00 +0800

    语音合成修改

--- app/src/main/java/com/example/mirry/baiduvoicedemo/MainActivity.java
+++ app/src/main/java/com/example/mirry/baiduvoicedemo/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.mirry.baiduvoicedemo;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -19,16 +26,28 @@ import com.baidu.speech.EventListener;
 import com.baidu.speech.EventManager;
 import com.baidu.speech.EventManagerFactory;
 import com.baidu.speech.asr.SpeechConstant;
+import com.baidu.tts.client.SpeechSynthesizer;
+import com.baidu.tts.client.SpeechSynthesizerListener;
+import com.baidu.tts.client.TtsMode;
+import com.example.mirry.baiduvoicedemo.config.InitConfig;
+import com.example.mirry.baiduvoicedemo.config.MySyntherizer;
+import com.example.mirry.baiduvoicedemo.config.NonBlockSyntherizer;
+import com.example.mirry.baiduvoicedemo.listener.UiMessageListener;
+import com.example.mirry.baiduvoicedemo.utils.OfflineResource;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity implements EventListener, View.OnClickListener {
+import static com.example.mirry.baiduvoicedemo.MainHandlerConstant.PRINT;
+import static com.example.mirry.baiduvoicedemo.MainHandlerConstant.UI_CHANGE_INPUT_TEXT_SELECTION;
+import static com.example.mirry.baiduvoicedemo.MainHandlerConstant.UI_CHANGE_SYNTHES_TEXT_SELECTION;
+
+public class MainActivity extends Activity implements EventListener, View.OnClickListener {
     Button online,offline,wakeupApp;
 
 
@@ -37,10 +56,35 @@ public class MainActivity extends AppCompatActivity implements EventListener, Vi
 
     private boolean enableOffline = true; // 测试离线命令词，需要改成true
 
+    String appId = "10502066";
+    String appKey = "TOcAhOFIQx2r3UnUSk5QtRHh";
+    String appSecret = "30e969283e06e3c15f0870bd92a53dce";
+
+    // TtsMode.MIX; 离在线融合，在线优先； TtsMode.ONLINE 纯在线； 没有纯离线
+    protected TtsMode ttsMode = TtsMode.MIX;
+
+    // 离线发音选择，VOICE_FEMALE即为离线女声发音。
+    // assets目录下bd_etts_speech_female.data为离线男声模型；bd_etts_speech_female.data为离线女声模型
+    protected String offlineVoice = OfflineResource.VOICE_MALE;
+
+    // 主控制类，所有合成控制方法从这个类开始
+    protected MySyntherizer synthesizer;
+
+    protected Handler mainHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                handle(msg);
+            }
+
+        };
 
         initPermission();
         initView();
@@ -61,6 +105,14 @@ public class MainActivity extends AppCompatActivity implements EventListener, Vi
         }
 
         startWakeUp();
+    }
+
+    protected void handle(Message msg) {
+        int what = msg.what;
+        switch (what) {
+            default:
+                break;
+        }
     }
 
     private void startWakeUp() {
@@ -165,6 +217,77 @@ public class MainActivity extends AppCompatActivity implements EventListener, Vi
 
     private void unloadOfflineEngine() {
         asr.send(SpeechConstant.ASR_KWS_UNLOAD_ENGINE, null, null, 0, 0);
+    }
+
+    protected void initialTts() {
+        // 设置初始化参数
+        SpeechSynthesizerListener listener = new UiMessageListener(mainHandler); // 此处可以改为 含有您业务逻辑的SpeechSynthesizerListener的实现类
+
+        Map<String, String> params = getParams();
+
+        // appId appKey secretKey 网站上您申请的应用获取。注意使用离线合成功能的话，需要应用中填写您app的包名。包名在build.gradle中获取。
+        InitConfig initConfig = new InitConfig(appId, appKey, appSecret, ttsMode, offlineVoice, params, listener);
+
+        synthesizer = new NonBlockSyntherizer(this, initConfig, mainHandler); // 此处可以改为MySyntherizer 了解调用过程
+    }
+
+    /**
+     * 合成的参数，可以初始化时填写，也可以在合成前设置。
+     *
+     * @return
+     */
+    protected Map<String, String> getParams() {
+        Map<String, String> params = new HashMap<String, String>();
+        // 以下参数均为选填
+        params.put(SpeechSynthesizer.PARAM_SPEAKER, "0"); // 设置在线发声音人： 0 普通女声（默认） 1 普通男声 2 特别男声 3 情感男声<度逍遥> 4 情感儿童声<度丫丫>
+        params.put(SpeechSynthesizer.PARAM_VOLUME, "5"); // 设置合成的音量，0-9 ，默认 5
+        params.put(SpeechSynthesizer.PARAM_SPEED, "5");// 设置合成的语速，0-9 ，默认 5
+        params.put(SpeechSynthesizer.PARAM_PITCH, "5");// 设置合成的语调，0-9 ，默认 5
+        params.put(SpeechSynthesizer.PARAM_MIX_MODE, SpeechSynthesizer.MIX_MODE_DEFAULT);         // 该参数设置为TtsMode.MIX生效。即纯在线模式不生效。
+        return params;
+    }
+
+    /**
+     * 切换离线发音。注意需要添加额外的判断：引擎在合成时该方法不能调用
+     */
+    private void loadModel() {
+        if (offlineVoice.equals(OfflineResource.VOICE_FEMALE)) {
+            offlineVoice = OfflineResource.VOICE_MALE;
+        } else {
+            offlineVoice = OfflineResource.VOICE_FEMALE;
+        }
+        int result = synthesizer.loadModel(offlineVoice);
+        checkResult(result, "loadModel");
+    }
+
+    private void checkResult(int result, String method) {
+        if (result != 0) {
+            Log.i("sss","error code :" + result + " method:" + method + ", 错误码文档:http://yuyin.baidu.com/docs/tts/122 ");
+        }
+    }
+
+    /**
+     * 暂停播放。仅调用speak后生效
+     */
+    private void pause() {
+        int result = synthesizer.pause();
+        checkResult(result, "pause");
+    }
+
+    /**
+     * 继续播放。仅调用speak后生效，调用pause生效
+     */
+    private void resume() {
+        int result = synthesizer.resume();
+        checkResult(result, "resume");
+    }
+
+    /*
+     * 停止合成引擎。即停止播放，合成，清空内部合成队列。
+     */
+    private void stop() {
+        int result = synthesizer.stop();
+        checkResult(result, "stop");
     }
 
     private void initPermission(){
